30c6625d08ec9e85316fb7e7f60aaec422c6b18b
Martin Mwabali <martinmwash18952058@gmail.com>
2025-10-14 15:52:05 +0300

    "Function for fetching the art and return in array form"

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const gallery = document.getElementById("artGallery");
 const artistSelect = document.getElementById("artistSelect");
+
+
+async function fetchArtworks() {
+    const response = await fetch("https://api.artic.edu/api/v1/artworks?limit=20");
+    const data = await response.json();
+    return data.data;
+}
